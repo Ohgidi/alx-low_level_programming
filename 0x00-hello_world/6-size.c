1f57771b79110b990bedac48f25524a8ae72a026
Ohgidi <ohgidi@yahoo.com>
2022-06-30 13:49:25 -0700

    seven commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+        char charType;
+        int intType;
+        long int longIntType;
+        long long int longLongType;
+        float floatType;
+	
+	printf("Size of a char: %1d byte(s)\n", sizeof(charType));
+	printf("Size of an int: %id byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %id byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %id byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %id byte(s)\n", sizeof(floatType));
+	return (0);
+}
