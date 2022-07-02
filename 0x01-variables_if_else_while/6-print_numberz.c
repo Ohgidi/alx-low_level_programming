1f02f06bcadc90d03a371fb02655e0ce787968e1
Ohgidi <ohgidi@yahoo.com>
2022-07-01 20:06:48 -0700

    my seven commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - output all single digit number of base 10
- *
- * Return: 0 if successful
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
+ * Return: 0
  */
 int main(void)
 {
-	int var;
+	int i;
 
-	for (var = 0; var < 10; var++)
-         	putchar((var % 10) + '0');
-
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
 	putchar('\n');
 
 	return (0);
