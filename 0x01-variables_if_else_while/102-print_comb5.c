93d1a22d3ee297a7e8466710c28e261fca7d5549
Ohgidi <ohgidi@yahoo.com>
2022-07-03 19:01:21 -0700

    my thirteen commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
-
 /**
- *  * main - output all single digit number of base 10
- *   *
- *    * Return: 0 if successful
- *     */
+ *  * main - main block
+ *   * Description: Print all possible combinations of two two-digit numbers.
+ *    * Numbers should range from 0 to 99.
+ *     * The two numbers should be separated by a space.
+ *      * All numbers should be printed with two digits. 1 should be printed as 01.
+ *       * Combination of numbers must be separated by a comma followed by a space.
+ *        * Combinations of numbers should be printed in ascending order.
+ *         * `00 01` and `01 00` are considered as the same combination.
+ *          * You can only use `putchar` to print to console.
+ *           * You can only use `putchar` up to 8 times.
+ *            * You are not allowed to use any variable of type `char`.
+ *             * Return: 0
+ *              */
 int main(void)
 {
-		int var;
+		int i, j;
+			int a, b, c, d;
 
-			for (var = 0; var < 99; var++)
-					{
-								int ii;
+				for (i = 0; i < 100; i++)
+						{
+									a = i / 10; /* doubles fnum */
+											b = i % 10; /* singles fnum */
 
-										for (ii = var + 1; ii < 100; ii++)
-													{
-																	putchar((var / 10) + '0');
-																				putchar((var % 10) + '0');
-																							putchar(' ');
-																										putchar((ii / 10) + '0');
-																													putchar((ii % 10) + '0');
+													for (j = 0; j < 100; j++)
+																{
+																				c = j / 10; /* doubles snum */
+																							d = j % 10; /* singles snum */
 
-																																if (var != 98)
-																																				{
-																																									putchar(',');
-																																													putchar(' ');
-																																																}
-																																			else
-																																								continue;
-																																					}
-											}
+																										if (a < c || (a == c && b < d))
+																														{
+																																			putchar(a + '0');
+																																							putchar(b + '0');
+																																											putchar(32);
+																																															putchar(c + '0');
+																																																			putchar(d + '0');
 
-				putchar('\n');
+																																																							if (!(a == 9 && b == 8))
+																																																												{
+																																																																		putchar(44);
+																																																																							putchar(32);
+																																																																											}
+																																																										}
+																												}
+														}
+					putchar(10);
 
-					return (0);
+						return (0);
 }
