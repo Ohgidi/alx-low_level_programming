4b509cb2278ec623afd42a268d89f7245738e2f4
Ohgidi <ohgidi@yahoo.com>
2022-07-13 02:21:27 -0700

    my four commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line.
- * @str: input string.
- * Return: no return.
+ * _puts -> this is a function that puts
+ * @str: a param to _puts function
  */
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
+	for (; *str != '\0' ; str++)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(*str);
 	}
+	_putchar('\n');
 }
+
