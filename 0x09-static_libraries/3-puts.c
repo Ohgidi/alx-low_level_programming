3700636a4e72b671a96685f727d7cb431e8637cc
Ohgidi <ohgidi@yahoo.com>
2022-07-24 06:11:19 -0700

    my first commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
+ * _puts - prints a string
+ * @str: string to be printed
+ *
+ * cc: 12th july, 2022.
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
