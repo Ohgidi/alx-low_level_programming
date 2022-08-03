a943708e7c31f28bb0baa7e45a1b254c920f497c
Ohgidi <ohgidi@yahoo.com>
2022-08-02 20:47:30 -0700

    my five commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,43 +1,49 @@
-#include <string.h>
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * new_dog - create new data structure for dog
- * @name: name of dog
- * @age: age of dog
- * @owner: dog owner
- *
- * Return: Pointer to new dog
- **/
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int len_name, len_owner;
+	struct dog *doge;
+	int i, j, k;
+	char *n, *o;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	doge = malloc(sizeof(struct dog));
+	if (doge == NULL)
 		return (NULL);
 
-	len_name = strlen(name);
-	new_dog->name = malloc(sizeof(char) * len_name + 1);
-	if (new_dog->name == NULL)
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+
+	n = malloc(sizeof(char) * i + 1);
+	if (n == NULL)
 	{
-		free(new_dog);
+		free(doge);
 		return (NULL);
 	}
-	new_dog->name = strcpy(new_dog->name, name);
-	len_owner = strlen(owner);
-	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
-	if (new_dog->owner == NULL)
+	o = malloc(sizeof(char) * j + 1);
+	if (o == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(n);
+		free(doge);
 		return (NULL);
 	}
+	for (k = 0; k <= i; k++)
+		n[k] = name[k];
+	for (k = 0; k <= j; k++)
+		o[k] = owner[k];
 
-	new_dog->owner = strcpy(new_dog->owner, owner);
-	new_dog->age = age;
+	doge->name = n;
+	doge->age = age;
+	doge->owner = o;
 
-	return (new_dog);
+	return (doge);
 }
